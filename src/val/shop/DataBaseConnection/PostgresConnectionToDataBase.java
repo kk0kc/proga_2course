b4194a867e38bda4fef9c24f569546f850674094
Elizaveta Goldova <lizagoldova13@gmail.com>
2022-11-09 19:02:43 +0300

    init

---
+++ src/val/shop/DataBaseConnection/PostgresConnectionToDataBase.java
@@ -0,0 +1,23 @@
+package val.shop.DataBaseConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgresConnectionToDataBase {
+    public static Connection getConnection() {
+
+        try {
+            Class.forName(PostgresPropereties.DRIVER);
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+        try {
+            Connection connection = DriverManager.getConnection(PostgresPropereties.URL, PostgresPropereties.USERNAME, PostgresPropereties.PASSWORD);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/val/shop/servlet/CancelOrderServlet.java
@@ -0,0 +1,33 @@
+package val.shop.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import val.shop.DataBaseConnection.PostgresConnectionToDataBase;
+import val.shop.dao.OrderDao;
+
+@WebServlet("/cancel-order")
+public class CancelOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+			String id = request.getParameter("id");
+			if(id != null) {
+				OrderDao orderDao = new OrderDao(PostgresConnectionToDataBase.getConnection());
+				orderDao.cancelOrder(Integer.parseInt(id));
+			}
+			response.sendRedirect("orders.jsp");
+
+	}
+
+}
